d4bad9e5d4dc775f0c80036c8b2297a2acb89707
rbrown0504 <32039205+rbrown0504@users.noreply.github.com>
2020-08-08 18:02:52 -0700

    add default source memory. add last build memory functions.

--- src/creeps/builder.js
+++ src/creeps/builder.js
@@ -7,7 +7,6 @@ var builder = {
             DEPOSIT: 2,
             BUILD: 3
         };
-        //console.log(creep.name,' Store Capacity: ' + creep.store.getFreeCapacity());
         if (creep.memory.sourceRoom == undefined) {
             creep.memory.sourceRoom = creep.room.name;
         }
@@ -16,22 +15,51 @@ var builder = {
         if (creep.energy != 0 && creep.memory.lastAction == ACTIONS.BUILD) {
             continueBuild = true;
         }
-
-        if(creep.store.getFreeCapacity() > 0 && !continueBuild) {
+        //ADD A DEFAULT SOURCE IF ONE DOESN'T CURRENTLY EXIST
+        if (creep.memory.source == undefined) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-                creep.memory.lastAction = ACTIONS.HARVEST;
-            }
+            creep.memory.source = sources[0].id;
         }
-        else {
-            // here is the sayHello() prototype
-            creep.sayDebug();
-            var construction = creep.room.find(FIND_CONSTRUCTION_SITES);
-            console.log(creep.name,JSON.stringify(construction));            
-            if(creep.build(construction[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(construction[0]);
-                creep.memory.lastAction = ACTIONS.BUILD;
+        if(creep.store.getFreeCapacity() > 0 && !continueBuild) {
+            //go to default source
+            var source = creep.getObject(creep.memory.source);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+                creep.memory.lastAction = ACTIONS.HARVEST;
+                creep.say('Harvesting');
+            }
+        } else {
+            //if there is a last construction site in memory, go to that, otherwise, find one
+            if (creep.memory.lastBuild == undefined) {                
+                var construction = creep.room.find(FIND_CONSTRUCTION_SITES);                
+                if(creep.build(construction[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(construction[0]);
+                    creep.memory.lastAction = ACTIONS.BUILD;
+                    creep.memory.lastBuild = construction[0].id;
+                }
+            } else {
+                var getNCons = creep.getObject(creep.memory.lastBuild);
+                if (getNCons && creep.store[RESOURCE_ENERGY] != 0) {                    
+                    if(creep.build(getNCons) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(getNCons);
+                        creep.memory.lastAction = ACTIONS.BUILD;
+                        creep.memory.lastBuild = getNCons.id;
+                    }
+                } else if (creep.store[RESOURCE_ENERGY] > 0) {
+                    var construction = creep.room.find(FIND_CONSTRUCTION_SITES);                
+                    if(creep.build(construction[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(construction[0]);
+                        creep.memory.lastAction = ACTIONS.BUILD;
+                        creep.memory.lastBuild = construction[0].id;
+                    }
+                } else {
+                    creep.say('Empty');
+                    var sources = creep.room.find(FIND_SOURCES);
+                    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[0]);
+                        creep.memory.lastAction = ACTIONS.HARVEST;
+                    }
+                }
             }
         }
     },
