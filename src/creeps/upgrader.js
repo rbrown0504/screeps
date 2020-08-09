50244aeb52b0c6d9aaae20301f6aa4e2f1ee74c7
rbrown0504 <32039205+rbrown0504@users.noreply.github.com>
2020-08-08 20:28:42 -0700

    added leveled build logic.

--- src/creeps/upgrader.js
+++ src/creeps/upgrader.js
@@ -45,7 +45,7 @@ var roleUpgrader = {
         }
     },
     // checks if the room needs to spawn a creep
-    spawn: function(room) {
+    spawn: function(room, level) {
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader' && creep.room.name == room.name);
         console.log('Upgraders: ' + upgraders.length, room.name);
 
@@ -54,12 +54,43 @@ var roleUpgrader = {
         }
     },
     // returns an object with the data to spawn a new creep
-    spawnData: function(room) {
+    spawnData: function(room, level) {
+            var abilities;
+            if(level <= 1) {
+                abilities = [WORK, CARRY, MOVE];
+            } else
+            if(level <= 2) {
+                abilities = [WORK, WORK, CARRY, MOVE];
+            } else
+            if(level <= 3) {
+                abilities = [WORK, WORK, CARRY, MOVE, MOVE];
+            } else
+            if(level <= 4) {
+                abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
+            } else
+            if(level <= 5) {
+                abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE];
+            } else
+            if(level <= 6) {
+                abilities = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+            } else
+            if(level <= 7) {
+                abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+            } else
+            if(level <= 8) {
+                abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+            } else
+            if(level <= 9) {
+                abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+            } else
+            if(level >= 10) {
+                abilities = [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+            }
             let name = 'Upgrader' + Game.time;
-            let body = [WORK, CARRY, MOVE];
+            //let body = [WORK, CARRY, MOVE];
             let memory = {role: 'upgrader'};
         
-            return {name, body, memory};
+            return {name, abilities, memory};
     }
 };
 
