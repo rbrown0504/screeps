c12bff64d0524cdf0d5fe10d74ebd54acde2d29c
rbrown0504 <32039205+rbrown0504@users.noreply.github.com>
2020-08-10 21:10:43 -0700

    Updates to Resource object. Added source memory in. logic for tracking containers near energy sources.

--- src/room/resources.js
+++ src/room/resources.js
@@ -12,43 +12,59 @@ function resources(room) {
 
                 return false;
             }
-    });
-    //room.memory.sources = sources;
+    });    
+    if (room.memory.sources == undefined) {
+        room.memory.sources = sources;        
+        
+    }        
     //go through sources and find structures (containers) that are in 3 range
-    sourcesInRangeToContainer = new Array();    
-    var sourceMap = new Map();    
-    _.forEach(sources, function(source) {
-        var sourceDetails = {
-            hasContainer : false,
-        };
+    var sourcesInRangeToContainer = new Array();                  
+    _.forEach(room.memory.sources, function(source) {
+        if (source.containersNear == undefined) {            
+            var newArray = new Array();
+            source.containersNear = newArray;
+        }        
+        var theSource = Game.getObjectById(source.id);
         //hasContainer = true IF CONTAINER IS FOUND BY A SOURCE
-        var targets = source.pos.findInRange(FIND_STRUCTURES, 3);
+        var targets = theSource.pos.findInRange(FIND_STRUCTURES, 3);
         _.forEach(targets, function(target) {
             if (target.structureType == STRUCTURE_CONTAINER) {
-                sourcesInRangeToContainer.push(source.id);                
-                sourceDetails.hasContainer = true;
-            }
+                sourcesInRangeToContainer.push(source.id);
+                source.hasContainer = true;
+                if (!source.containersNear.includes(target.id)) {
+                    source.containersNear.push(target.id);
+                    var existing = source.containersNear;
+                    existing.push(target.id);
+                    var unique = existing.filter((v, i, a) => a.indexOf(v) === i);       
+                    source.containersNear = unique;
+                }
+            };
         });
         //hasContainer = true IF THERE IS A CONTAINER BEING CONSTRUCTED BY A SOURCE
-        var targets = source.pos.findInRange(FIND_CONSTRUCTION_SITES, 3);
+        var targets = theSource.pos.findInRange(FIND_CONSTRUCTION_SITES, 3);
         _.forEach(targets, function(target) {
             if (target.structureType == STRUCTURE_CONTAINER) {
-                sourcesInRangeToContainer.push(source.id);                
-                sourceDetails.hasContainer = true;
+                sourcesInRangeToContainer.push(source.id);
+                source.hasContainer = true;
+                if (!source.containersNear.includes(target.id)) {
+                    source.containersNear.push(target.id);
+                    var existing = source.containersNear;
+                    existing.push(target.id);
+                    var unique = existing.filter((v, i, a) => a.indexOf(v) === i);       
+                    source.containersNear = unique;
+                }       
             }
         });
-        sourceMap.set(source.id,sourceDetails);
     });
     //rule: there must be at least on container within 3 range of each energy source
     console.log('Sources in range (3) of a container ' + sourcesInRangeToContainer + ' sources length ' + sources.length + ' sources container length ' + sourcesInRangeToContainer.length);
+    
     if (sources.length > sourcesInRangeToContainer.length) {
         //console.log('need to build a container');        
         var sourcesNeedingContainer = new Array();
         _.forEach(sources, function(source) {
             if (!sourcesInRangeToContainer.includes(source.id)) {
                 sourcesNeedingContainer.push(source.id);
-                // console.log('sssssssssssssss ' + JSON.stringify(sourceMap.get(source.id)));
-                // console.log('sourceDetail: ' + JSON.stringify(sourceMap.get(source.id).hasContainer));
             }                       
         });
         if (sourcesNeedingContainer.length > 0) {
@@ -61,6 +77,8 @@ function resources(room) {
     } else if (sources.length == sourcesInRangeToContainer.length) {
         console.log('there is at least one container next to each source');
     }
+
+
     // var towers = room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER }});
     // if (towers.length) {
     //     _.forEach(towers, function(tower) {
