2c810fdc82cdaa154c3cf8785c04b8efd81eb10c
rbrown0504 <32039205+rbrown0504@users.noreply.github.com>
2020-08-12 21:19:55 -0700

    Reorganized code.

--- src/room/index.js
+++ src/room/index.js
@@ -1,6 +1,8 @@
 let roomLogic = {
     spawning:     require('./spawning'),
     defense:     require('./defense'),
+    resources:     require('./resources'),
+    construction:     require('./construction'),
 }
 
 module.exports = roomLogic;
