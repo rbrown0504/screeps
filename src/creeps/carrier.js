ac8cd13fb87094ce7ef8f7e2fa991dfb1013dd77
rbrown0504 <32039205+rbrown0504@users.noreply.github.com>
2020-08-16 11:31:21 -0700

    Carrier Role Updates. Spawning Updates #7 #3

--- src/creeps/carrier.js
+++ src/creeps/carrier.js
@@ -44,7 +44,6 @@ var carrier = {
         }
 
         var deposits = creep.getOpenDeposits();
-
         var spawns = [];
         for(var n in Game.spawns) { 
             var s = Game.spawns[n];
@@ -56,67 +55,54 @@ var carrier = {
         if(deposits == 0 && spawns[0].energy == spawns[0].energyCapacity) {
             //help a builder out
             depositFor = DEPOSIT_FOR.CONSTRUCTION;
-            //console.log('it would do this.. deposit for construction');
         } else {
             //go to containers or spawn
             depositFor = DEPOSIT_FOR.POPULATION;
-            //console.log('it would do this.. deposit for population');
         }        
 
         //start doing stuff
         if(creep.store.getFreeCapacity() > 0 && creep.memory.lastAction != ACTIONS.DEPOSIT) {
-            console.log('111111111111111111111111111111');
             var containers = creep.getHarvestContainers();
-            console.log('harvesContainers: ',containers);
+            //console.log('harvesContainers: ',containers);
             if (containers.length > 0) {
-                console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
                 creep.harvestContainer(containers[0],ACTIONS);
             }
         } else if (creep.store[RESOURCE_ENERGY] == 0) {
             creep.say('Empty');
-            console.log('222222222222222222222222222222');
             //get energy from container
             var containers = creep.getHarvestContainers();
-            console.log('CARRIER: harvestContainers',containers,creep.harvestContainer(containers[0],ACTIONS));
-            if (containers > 0) {
+            if (containers.length > 0) {
 
                 creep.harvestContainer(containers[0],ACTIONS);
             }            
         } else if (creep.memory.lastAction == ACTIONS.DEPOSIT) {
             creep.deposit(deposits[0],ACTIONS);
-            console.log('333333333333333333333333333333');
         } else if (creep.memory.lastAction == ACTIONS.HARVEST) {
-            console.log('aaaa444444444444444444444444444444444444');
-
-
             var containers = creep.getHarvestContainers();
 
             if (creep.store.getFreeCapacity() == 0) {
-                console.log('CARRIER: Deposits',JSON.stringify(deposits),'firstREsult',JSON.stringify(deposits[0]));
                 if (deposits.length > 0) {
-                    console.log('CARRIER: ACTION' , creep.deposit(deposits[0],ACTIONS));
                     creep.deposit(deposits[0],ACTIONS);
-                    console.log('CARRIER: Deposit');
                 } else {
                     console.log('CARRIER: Deposits Full');
                 }                                
-            } else {                
-                creep.harvestContainer(containers[0],ACTIONS);
-                console.log('ddddddddddddddddddd');
+            } else {
+                if (containers.length > 0) {
+                    creep.harvestContainer(containers[0],ACTIONS);
+                }                                             
             }
         } else if (creep.store[RESOURCE_ENERGY] == 0) {
-            console.log('555555555555555555555555555555555555');
             creep.say('Empty');
             //get energy from container
             var containers = creep.getHarvestContainers();
 
-            if (containers > 0) {
+            if (containers.length > 0) {
                 creep.harvestContainer(containers[0],ACTIONS);
             }            
         } else {   
-            console.log('6666666666666666666666666666666666');
+            //console.log('6666666666666666666666666666666666');
             var containers = creep.getHarvestContainers();
-            if (containers > 0) {
+            if (containers.length > 0) {
                 creep.harvestContainer(containers[0],ACTIONS);
             }         
         }                
@@ -124,14 +110,11 @@ var carrier = {
     },
     // checks if the room needs to spawn a creep
     spawn: function(room, level, roleDistribution) {
-        var carriers = _.filter(Game.creeps, (creep) => creep.memory.role == 'carrier' && creep.room.name == room.name);
-        //number of deposits
-        //var depositContainers =  this.getDepositContainers();
-        //console.log('spawncarriers: ', 'depositNeeded: ', room.memory.depositNeeded);
+        //var carriers = _.filter(Game.creeps, (creep) => creep.memory.role == 'carrier' && creep.room.name == room.name);        
         var min = roleDistribution.min;
         switch(room.memory.numberExtensions) {
             case 0:
-                min = 2;
+                min = 0;
                 break;
             case 1:
                 min = 2;
@@ -140,7 +123,7 @@ var carrier = {
                 min = 4;
                 break;
             case 3:
-                min = 4;
+                min = 6;
                 break;
             case 4:
                 min = 6;
@@ -149,7 +132,29 @@ var carrier = {
                 min = 6;
                 break;                        
         }
-        console.log('carriers: ' + roleDistribution.total, room.name);        
+
+        switch(room.memory.totalContainers) {
+            case 0:
+                min = 0;
+                break;
+            case 1:
+                min = 2;
+                break;
+            case 2:
+                min = 4;
+                break;
+            case 3:
+                min = 6;
+                break;
+            case 4:
+                min = 8;
+                break;            
+            case 5:
+                min = 10;
+                break;                        
+        }
+        
+        //console.log('carriers: ' + roleDistribution.total, room.name);        
         if (roleDistribution.total < min
             && room.memory.numberExtensions >= roleDistribution.minExtensions 
             && roleDistribution.total <= roleDistribution.max) {
@@ -161,43 +166,43 @@ var carrier = {
             let name = 'carrier' + Game.time;
             let memory = {role: 'carrier'};
             if(level <= 1) {
-                let body = [WORK, CARRY, MOVE];
+                let body = [CARRY, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 2) {
-                let body = [WORK, WORK, CARRY, MOVE];
+                let body = [CARRY, CARRY, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 3) {
-                let body = [WORK, WORK, CARRY, MOVE, MOVE];
+                let body = [CARRY, CARRY, MOVE, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 4) {
-                let body = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
+                let body = [CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 5) {
-                let body = [WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE];
+                let body = [CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 6) {
-                let body = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+                let body = [CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 7) {
-                let body = [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+                let body = [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 8) {
-                let body = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+                let body = [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
                 return {name, body, memory};
             } else
             if(level <= 9) {
-                let body = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+                let body = [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
                 return {name, body, memory};
             } else
             if(level >= 10) {
-                let body = [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+                let body = [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,  CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
                 return {name, body, memory};
             }
     }
